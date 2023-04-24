17e286bb5901270ab4f71e4095d1081451f96c40
simon GL <simon.gl3135@orange.fr>
2023-04-24 20:07:23 +0200

    fix : norme

--- src/update_angle.c
+++ src/update_angle.c
@@ -12,28 +12,22 @@ static void get_angle(main_t *main, float distance)
     if (distance >= 1500){
         main->angle = 0.5;
         return;
-    }
-    if (distance >= 1000){
+    } if (distance >= 1000){
         main->angle = 0.07;
         return;
-    }
-    if (distance >= 600){
+    } if (distance >= 600){
         main->angle = 0.1;
         return;
-    }
-    if (distance >= 400){
+    } if (distance >= 400){
         main->angle = 0.4;
         return;
-    }
-    if (distance >= 200){
+    } if (distance >= 200){
         main->angle = 0.8;
         return;
-    }
-    if (distance < 200 && distance >= 50){
+    } if (distance < 200 && distance >= 50){
         main->angle = 0.9;
         return;
-    }
-    else
+    } else
         main->angle = 1;
 }
 
@@ -42,7 +36,7 @@ void update_angle(main_t *main)
     if (len_of_array(main->lidar) < 15)
         return;
     float distance = atof(main->lidar[17]);
-    float direction = (atof(main->lidar[3]) - atof(main->lidar[31]) < 0) ? -1 : 1;
+    float dir = (atof(main->lidar[3]) - atof(main->lidar[31]) < 0) ? -1 : 1;
     get_angle(main, distance);
-    main->angle *= direction;
+    main->angle *= dir;
 }
