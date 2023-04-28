6fa98c26c9aa7cb058755b885caf4de9286caedd
6im0n <simon.gl3135@orange.fr>
2023-04-28 12:55:45 -0400

    update: if car is stuck to a wall it go bacyard

--- src/main.c
+++ src/main.c
@@ -7,6 +7,38 @@
 
 #include "n4s.h"
 
+static void get_info(car_t *car)
+{
+    char *line = NULL;
+    size_t len = 0;
+
+    dprintf(1, "GET_INFO_LIDAR\n");
+    getline(&line, &len, stdin);
+    car->lidar = split_str(line, ':');
+}
+
+static void fail(car_t *car)
+{
+    size_t n = 0;
+    char *buffer = NULL;
+    float dis_f = atof(car->lidar[17]);
+    float dis_r = atof(car->lidar[3]);
+    float dis_l = atof(car->lidar[31]);
+
+    if (dis_f < 100 || dis_r < 100 || dis_l < 100){
+        car->angle = -(car->angle);
+        turn(car);
+        dprintf(2, "#CAR_BACKWARDS\n");
+        dprintf(1, "CAR_BACKWARDS:0.3\n");
+        getline(&buffer, &n, stdin);
+        car->speed = -0.5;
+        car->angle = -(car->angle);
+        get_info(car);
+        fail(car);
+    }
+    return;
+}
+
 static void init_struct(car_t *car)
 {
     car->speed = 0.5;
@@ -16,27 +48,17 @@ static void init_struct(car_t *car)
     car->direction = 0;
 }
 
-static void get_info(car_t *car)
-{
-    char *line = NULL;
-    size_t len = 0;
-    int read = 0;
-    dprintf(2, "#GET_INFO_LIDAR\n");
-    dprintf(1, "GET_INFO_LIDAR\n");
-    read = getline(&line, &len, stdin);
-    if (read == -1)
-        exit(84);
-    car->lidar = split_str(line, ':');
-}
-
 static void start_simulation(void)
 {
     size_t n = 0;
     char *buffer = NULL;
+    int read = 0;
 
     dprintf(2,"#START_SIMULATION\n");
     dprintf(1,"START_SIMULATION\n");
-    getline(&buffer, &n, stdin);
+    read = getline(&buffer, &n, stdin);
+    if (read == -1)
+        exit(84);
 }
 
 int main(void)
@@ -48,6 +70,7 @@ int main(void)
         get_info(&car);
         update_speed(&car);
         update_angle(&car);
+        fail(&car);
         forward(&car);
         turn(&car);
         track_cleared(&car);
