ebfd09cd07edfc7b23ec2c9da54e5a9e95543667
6im0n <simon.gl3135@orange.fr>
2023-04-28 18:39:51 -0400

    fix : norme

--- src/displayer.c
+++ src/displayer.c
@@ -9,5 +9,6 @@
 
 void display_info(car_t *car)
 {
-    dprintf(2, "\e[1;32mSpeed:\e[0m %d \e[1;32mKm/H\e[0m\r", car->display_speed);
+    dprintf(2, "\e[1;32m Speed:\e[0m %d \e[1;32m Km/H\e[0m\r",
+    car->display_speed);
 }
